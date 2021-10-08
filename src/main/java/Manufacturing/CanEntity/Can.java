716896ffa95d9ac75b7bc46bfdb35793d84a526f
Qiao Liang <19921305202@163.com>
2021-10-08 08:59:20 +0800

    完成了部分观察者模式

--- src/main/java/Manufacturing/CanEntity/Can.java
+++ src/main/java/Manufacturing/CanEntity/Can.java
@@ -1,5 +1,15 @@
 package Manufacturing.CanEntity;
 
+
+
 public abstract class Can {
+    public String CanName = new String();
+    public float Price;
+    public float Weight;
+    Can(String Name,float Price,float weight ){
+        this.CanName = Name;
+        this.Price = Price;
+        this.Weight = weight;
+    }
 
 }
