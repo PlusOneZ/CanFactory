83278f2b6df03272295def6689893f6d2774c015
mountPOTATO <503253721@qq.com>
2021-10-12 09:00:59 +0800

    feat:组合模式，命令模式，责任链模式

--- src/main/java/Management/HumanResources/Command/Command.java
+++ src/main/java/Management/HumanResources/Command/Command.java
@@ -2,10 +2,10 @@ package Management.HumanResources.Command;
 
 /**
  * 命令模式下的命令基类
+ * <b>命令模式的一部分</b>
  * @author 陈垲昕
- *
+ * @since 2021-10-12 9:00 上午
  */
-
 public abstract class Command {
 
     /**
