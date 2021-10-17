cb51c62449cfb3971e4714b7fc98ce2889f3e7c7
olivershang <olivershang0017@gmail.com>
2021-10-17 11:29:56 +0800

    已添加 https://github.com/PlusOneZ/CanFactory/pull/27#discussion_r730271751

--- src/main/java/Management/HumanResources/BaseDepartment.java
+++ src/main/java/Management/HumanResources/BaseDepartment.java
@@ -1,6 +1,8 @@
 package Management.HumanResources;
 
 import Management.HumanResources.Manager.Manager;
+import Presentation.Protocol.OutputManager;
+
 
 import java.util.List;
 
@@ -15,19 +17,37 @@ public abstract class BaseDepartment {
 
     protected List<BaseEmployee> employees;
 
-    protected Manager manager;
+    protected Manager manager = null;
 
     protected DepartmentType type = DepartmentType.None;
 
+    /**
+     * 将员工注册到相应的部门
+     * @param employee
+     */
     public void register(BaseEmployee employee){
         employees.add(employee);
         employee.setDepartment(type);
+
+        if(manager == null && (employee instanceof Manager)){
+            this.manager = (Manager)employee;
+        }
     }
 
-    public void setManager(Manager manager){
-        // TODO: 判断该部门是否已经有经理
-
-        manager = manager;
+    /**
+     * 设置该部门的经理
+     * @param manager
+     */
+    public void setManager(Manager manager) {
+        if (manager != null) {
+            manager = manager;
+        }
+        else{
+            OutputManager.getInstance().errorMassage(
+                    "该部门已有经理，请勿重复设置",
+                    "該部門已有經理，請勿重複設置",
+                    "Manager already exists."
+                    );
+        }
     }
-
 }
