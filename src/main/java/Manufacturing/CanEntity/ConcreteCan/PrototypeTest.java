865763e7a9e6a7b75f3c08fecaed8af3f1c06043
汪汪汪 <1209497940@qq.com>
2021-11-02 13:32:58 +0800

    添加了原型模式测试类

--- src/main/java/Manufacturing/CanEntity/ConcreteCan/PrototypeTest.java
+++ src/main/java/Manufacturing/CanEntity/ConcreteCan/PrototypeTest.java
@@ -1,5 +1,11 @@
 package Manufacturing.CanEntity.ConcreteCan;
 
+import Manufacturing.CanEntity.Can;
+import Manufacturing.ProductLine.AbstractCanFactory.GlassCanFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 原型模式测试类
  *
@@ -7,4 +13,12 @@ package Manufacturing.CanEntity.ConcreteCan;
  * @date 2021/11/2 13:23
  */
 public class PrototypeTest {
+    public static void main(String[] args) {
+        List<Can> cans = new ArrayList<>();
+
+        for(int i=0;i<10;++i){
+            cans.add(GlassCanFactory.getInstance().createBigCan("Peach"));
+        }
+        
+    }
 }
